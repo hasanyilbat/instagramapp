dda88c6d7d0adb3bb02cec2f57c9d533f8e994f6
Hasan <hasan.yilbat@gmail.com>
2022-09-14 21:22:47 +0300

    instagram clone starter

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useRef } from "react";
+import { useEffect } from "react";
 
 function App() {
+  const ref = useRef();
+
+  useEffect(() => {
+    let images = ref.current.querySelectorAll("img");
+    const total = images.length;
+    let current = 0;
+
+    const imageSlider = () => {
+      if (current > 0) {
+        images[current - 1].classList.add("opacity-0");
+      } else {
+        images[total - 1].classList.add("opacity-0");
+      }
+      images[current].classList.remove("opacity-0");
+      if (current === total - 1) {
+        current = 0;
+      } else {
+        current += 1;
+      }
+    };
+    let interval = setInterval(imageSlider, 3000);
+    return () => {
+      clearInterval(interval);
+    };
+  }, [ref]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="h-full w-full flex items-center justify-center " ref={ref}>
+      <div className="w-[380px] h-[581px] relative  bg-logo-pattern bg-length:[468.32px_634.15px] bg-[top_left_-46px]">
+        <div className="w-[250px] h-[581px] absolute top-[27px] right-[18px]">
+          <img
+            className="absolute top-0 left-0 opacity-0 transition-opacity duration-750 ease-linear"
+            src="https://www.instagram.com/static/images/homepage/screenshots/screenshot1.png/fdfe239b7c9f.png"
+            alt=""
+          />
+          <img
+            className="absolute top-0 left-0 opacity-0 transition-opacity duration-700 ease-linear"
+            src="https://www.instagram.com/static/images/homepage/screenshots/screenshot2.png/4d62acb667fb.png"
+            alt=""
+          />
+          <img
+            className="absolute top-0 left-0 opacity-0 transition-opacity duration-700 ease-linear"
+            src="https://www.instagram.com/static/images/homepage/screenshots/screenshot3.png/94edb770accf.png"
+            alt=""
+          />
+          <img
+            className="absolute top-0 left-0 opacity-0 transition-opacity duration-700 ease-linear"
+            src="https://www.instagram.com/static/images/homepage/screenshots/screenshot4.png/a4fd825e3d49.png"
+            alt=""
+          />
+        </div>
+      </div>
     </div>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,jsx}"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "logo-pattern":
+          "url(https://www.instagram.com/static/images/homepage/phones/home-phones.png/1dc085cdb87d.png)",
+      },
+    },
+  },
+  plugins: [],
+};
